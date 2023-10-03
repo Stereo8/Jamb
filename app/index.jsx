9e4838e3fb2dd534e95b28cbd3e9a2584184b6b3
Vid <plavsic.vid@gmail.com>
2023-10-03 19:24:59 +0200

    feat: :sparkles: :heavy_plus_sign: add expo router and tailwind
    Sets-up Expo router with native Stack layout. Adds Tailwind support through Nativewind

---
+++ app/index.jsx
@@ -0,0 +1,39 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function MainMenu() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Jamb</Text>
+      <View style={styles.divider}></View>
+      <View style={styles.menu}>
+        <Button title="Nastavi" />
+        <Button title="Nova igra" />
+        <Button title="Istorija" />
+        <Button title="Pomoć" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  menu: {
+    flexDirection: "column",
+    flex: 1,
+  },
+  title: {
+    fontSize: 96,
+    marginTop: 96,
+  },
+  divider: {
+    flexGrow: 1,
+  },
+});
