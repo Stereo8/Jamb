55dde295cd6ec6f237ea205c10aebe8a0d58f9da
Vid <plavsic.vid@gmail.com>
2023-10-05 13:11:48 +0200

    feat: :sparkles: highlight playable fields in Column

--- components/column.tsx
+++ components/column.tsx
@@ -1,44 +1,70 @@
 import { IColumn } from "../stores/column";
 import { ColumnData } from "../types/columnData";
 import { Text, View, StyleSheet } from "react-native";
+import { observer } from "mobx-react";
 
 type Props = { column: IColumn };
 
-export const Column = (props: Props) => {
-  const openFieldEntryModal = () => {
-    // TODO
+export const Column = observer((props: Props) => {
+  const openFieldEntryModal = (field: string) => {
+    props.column.setField(field, 38);
   };
 
   const columnFields = Object.entries(props.column.columnData).map(
     // @ts-ignore
-    ([field, value]) => (
-      <Text
-        key={field}
-        style={styles.text}
-        onPress={() => {
-          openFieldEntryModal();
-        }}
-      >
-        {`${field}: ${value ?? ""}`}
-      </Text>
-    )
+    ([field, value]) => {
+      return (
+        <View
+          key={field}
+          style={{
+            ...styles.cell,
+            ...(props.column.playableFields.includes(field) &&
+              styles.highlighted),
+          }}
+        >
+          <Text
+            style={styles.text}
+            onPress={() => {
+              openFieldEntryModal(field);
+            }}
+          >
+            {`${value ?? ""}`}
+          </Text>
+        </View>
+      );
+    }
   );
   return <View style={styles.column}>{columnFields}</View>;
-};
+});
 
 const styles = StyleSheet.create({
   column: {
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
   },
   text: {
     color: "#000",
-    fontSize: 24,
-    padding: 8,
+    fontSize: 18,
+  },
+  cell: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     borderColor: "#000",
-    borderWidth: 2,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
     borderStyle: "solid",
-    width: 130,
+    maxWidth: 40,
+    maxHeight: 40,
+    minWidth: 40,
+    minHeight: 40,
+  },
+  highlighted: {
+    backgroundColor: "#bbedbf",
   },
 });
